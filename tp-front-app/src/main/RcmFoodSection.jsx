3a8c543e5f47bf7d8c3431fea011dcfa65483389
HANAH-J <hana21k@naver.com>
2023-11-12 22:35:04 +0900

    design: 음식점 추천 작업
    - 음식점 추천 영역 CSS 수정

--- tp-front-app/src/main/RcmFoodSection.jsx
+++ tp-front-app/src/main/RcmFoodSection.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import RcmCard from "./rcm/RcmCard";
 import { useRecoilValue } from "recoil";
 import { rcmFoodListState } from "../state/main/MainState";
+import styles from "../css/main/Main.module.css";
 
 export default function RcmFoodSection() {
     const rcmFoodList = useRecoilValue(rcmFoodListState);
 
     return (
-        <div className="main-rcm-food-area">
-            {rcmFoodList.map((item) => (
-                <RcmCard props={item} key={item.imageUrl} />
-            ))}
+        <div className={styles.rcm_food_wrap}>
+            <h2 className={styles.rcm_food_h2}>오늘의 인기 멍멍이</h2>
+            <span className={styles.rcm_food_span}>
+                다른 유저가 좋아한 멍멍이입니다.
+            </span>
+            <button className={styles.rcm_food_more_button}>더 보기</button>
+            <ul className={styles.rcm_food_list_wrap}>
+                {rcmFoodList.map((item) => (
+                    <RcmCard props={item} key={item.imageUrl} />
+                ))}
+            </ul>
         </div>
     );
 }
