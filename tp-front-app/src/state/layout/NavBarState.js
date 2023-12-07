e174410175222a5c90637cd26aca772d621f0bec
HANAH-J <hana21k@naver.com>
2023-12-07 21:54:29 +0900

    fix: 콘솔창 오류 수정
    Warning: Encountered two children with the same key
    - 컴포넌트 렌더링 시 고유한 key 값으로 id 지정

--- tp-front-app/src/state/layout/NavBarState.js
+++ tp-front-app/src/state/layout/NavBarState.js
@@ -4,54 +4,26 @@ const navListState = atom({
     key: "navListState",
     default: [
         {
+            id: 0,
             navName: "살펴보기",
             path: "/discover",
         },
         {
+            id: 1,
             navName: "여행",
             path: "/trips",
         },
         {
+            id: 2,
             navName: "커뮤니티",
             path: "/community",
         },
         {
+            id: 3,
             navName: "더 보기",
             path: "/more",
         },
     ],
 });
-
-// const filteredTodoListState = selector({
-//   key: "navListState",
-//   get: ({ get }) => {
-//     const filter = get(todoListFilterState);
-
-//     return <div>test1 Loading</div>
-//   }
-// });
-
-// const todoListStatsState = selector({
-//   key: "todoListStatsState",
-//   get: ({ get }) => {
-//     const todoList = get(todoListState);
-//     const totalNum = todoList.length;
-//     const totalCompletedNum = todoList.filter((item) => item.isComplete).length;
-//     let allText = "";
-//     todoList
-//       .filter((item) => !item.isComplete)
-//       .map((item) => (allText = allText + " " + item.text));
-//     const totalUncompletedNum = totalNum - totalCompletedNum;
-//     const percentCompleted = totalNum === 0 ? 0 : totalCompletedNum / totalNum;
-
-//     return {
-//       totalNum,
-//       totalCompletedNum,
-//       totalUncompletedNum,
-//       percentCompleted,
-//       allText
-//     };
-//   }
-// });
 
 export { navListState };
