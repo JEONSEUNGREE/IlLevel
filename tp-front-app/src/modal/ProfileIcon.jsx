a86b3ecc28e1f304ffe0d4451644a378694ddc05
HANAH-J <hana21k@naver.com>
2023-12-10 02:16:54 +0900

    fest: 권한에 따른 프로필 페이지 설정 작업
    - 일반 / 기업 회원 로그인 시 권한에 적합한 프로필 페이지 이동 기능 추가

--- tp-front-app/src/modal/ProfileIcon.jsx
+++ tp-front-app/src/modal/ProfileIcon.jsx
@@ -1,11 +1,20 @@
-import React from "react";
-import { useRecoilValue } from "recoil";
-import { iconListState } from "../state/modal/IconListState";
+import React, { useState, useEffect } from "react";
+import { userIconListState, comIconListState } from "../state/modal/IconListState";
 import styles from "../css/modal/Profile.module.css";
 import SettingForm from "../profile/SettingForm";
+import GetRole from "../sign/GetRole";
 
 export default function ProfileIcon() {
-    const statusList = useRecoilValue(iconListState);
+    const [statusList, setStatusList] = useState([]);
+    const role = GetRole();
+
+    useEffect(() => {
+        if (role === "ROLE_USER") {
+            setStatusList(userIconListState);
+        } else if (role === "ROLE_COM") {
+            setStatusList(comIconListState);
+        }
+    }, [role]);
 
     return (
         <div className={styles.setting_wrapper}>
